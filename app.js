60e032209ebb937db42757ab82e0221006301086
zayidanil <joy@cbncloud.co.id>
2018-08-02 09:52:19 +0700

    nodejs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = 'joyphp2.appservice.jkt.cbncloud.co.id';
-const port = 3000;
+const port = 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
